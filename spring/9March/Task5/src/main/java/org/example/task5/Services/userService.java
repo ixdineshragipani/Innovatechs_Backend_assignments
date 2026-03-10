c81848b64bb127c55274859da052e59cb138f8cd
ixdineshragipani <dinesh.ragipani@innovatechs.com>
2026-03-10 10:40:30 +0530

    CommandLineRunner used

--- spring/9March/Task5/src/main/java/org/example/task5/Services/userService.java
+++ spring/9March/Task5/src/main/java/org/example/task5/Services/userService.java
@@ -2,16 +2,22 @@ package org.example.task5.Services;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+
 @Service
-public class userService {
+public class userService implements CommandLineRunner {
 
     @Value("${app.name}")
     private String na;
 
-    @PostConstruct
-    public void userServices(){
+    @Value("${app.welcome.message}")
+    private String h;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(h+na.toLowerCase());
         System.out.println(na);
     }
 }
